d4c42560cfd5c4196708380d270ff3efa37a91f5
Rafael Cena <rafael.cena@hotmail.com>
2023-02-20 13:27:04 -0300

    css com stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
+import { styled } from "@/styles"
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  borderRadius:4,
+  border: 0,
+  padding: "8px 16px",
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  '&:hover': {
+    filter: "brightness(0.9)",
+  },
+})
 
 export default function Home() {
   return (
     <>
     <h1>Hello world</h1>
+    <Button> <span>TESTE</span>Botão novo</Button>
     </>
   )
 }
